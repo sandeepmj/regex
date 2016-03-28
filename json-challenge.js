31ad47c3fef475b6b94eb1e1d4219546efec992d
Sandeep <sjnews@gmail.com>
2016-03-28 15:03:27 -0400

    initial commit

---
+++ json-challenge.js
@@ -0,0 +1,23 @@
+var employees = [
+{
+"firstName": "Ramon, III",
+"lastName": "Martinez",
+"agency": "City Council",
+"salary": "$227,691",
+"title": "Chief Of Staff"
+},
+{
+"firstName": "John",
+"lastName": "Murphy",
+"agency": "Dept. of Buildings",
+"salary": "$217,725",
+"title": "Associate Inspector"
+},
+{
+"firstName": "Robert",
+"lastName": "Linn",
+"agency": "Office of Labor Relation",
+"salary": "$208,286",
+"title": "Commissioner Of Labor Relations"
+},
+]
