cd9c8ce316161a0670b8735a7b70ed65287f8a4a
Sandeep <sjnews@gmail.com>
2016-03-28 18:21:02 -0400

    final edits

--- json-challenge.js
+++ json-challenge.js
@@ -1,23 +1,26 @@
 var employees = [
 {
-"firstName": "Ramon, III",
-"lastName": "Martinez",
-"agency": "City Council",
-"salary": "$227,691",
-"title": "Chief Of Staff"
+ "rowID": 1,
+ "firstName": "Ramon",
+ "lastName": "Martinez",
+ "agency": "City Council",
+ "salary": "$227,691",
+ "title": "Chief Of Staff"
 },
 {
-"firstName": "John",
-"lastName": "Murphy",
-"agency": "Dept. of Buildings",
-"salary": "$217,725",
-"title": "Associate Inspector"
+ "rowID": 2,
+ "firstName": "John",
+ "lastName": "Murphy",
+ "agency": "Dept. of Buildings",
+ "salary": "$217,725",
+ "title": "Associate Inspector"
 },
 {
-"firstName": "Robert",
-"lastName": "Linn",
-"agency": "Office of Labor Relation",
-"salary": "$208,286",
-"title": "Commissioner Of Labor Relations"
+ "rowID": 3,
+ "firstName": "Robert",
+ "lastName": "Linn",
+ "agency": "Office of Labor Relation",
+ "salary": "$208,286",
+ "title": "Commissioner Of Labor Relations"
 },
 ]
